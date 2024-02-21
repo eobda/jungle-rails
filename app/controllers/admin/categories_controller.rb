89ff9a2b74c061ae49ca3b98f8c7f2fddaf9a76d
maddie <lee.maddie@gmail.com>
2024-02-21 12:24:03 -0500

    Add routes to controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,16 @@
 class Admin::CategoriesController < ApplicationController
+  # http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASS']
+  # commented out while building page
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+  end
+
 end
