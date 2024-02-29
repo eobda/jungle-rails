cd65bebee69b447a8306869fb86b06fd3ea33ed7
maddie <lee.maddie@gmail.com>
2024-02-28 22:06:05 -0500

    Remove unique email migration

--- db/migrate/20240229023929_make_user_emails_unique.rb
+++
@@ -1,5 +0,0 @@
-class MakeUserEmailsUnique < ActiveRecord::Migration[6.1]
-  def change
-    change_column :users, :email, :string, unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2024_02_29_023929) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email", unique: true
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
