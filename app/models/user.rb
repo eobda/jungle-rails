1c74fc4ae271b61d345064f9e32cb820d13d1113
maddie <lee.maddie@gmail.com>
2024-02-28 22:45:42 -0500

    Add presence valiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
-  validates :email, uniqueness: { case_sensitive: false }
   validates :password, confirmation: true
 end
