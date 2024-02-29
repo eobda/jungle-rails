097772597e421c444599a2ff17e8240346e238f0
maddie <lee.maddie@gmail.com>
2024-02-28 21:13:37 -0500

    Delete comments and rephrase test title for clarity

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # initial example
-    it 'ensures a product with all four fields set will save successfully' do
+    it 'successfully saves a product with all four fields set' do
       category = Category.new(name: 'x')
       product = Product.new(name: 'y', price_cents: 500, quantity: 10, category: category)
       expect(product).to be_valid
     end
 
-    # validates :name, presence: true
     it 'returns an error when :name is not present' do
       category = Category.new(name: 'x')
       product = Product.new(name: nil, price_cents: 500, quantity: 10, category: category)
@@ -17,7 +15,6 @@ RSpec.describe Product, type: :model do
       expect(product.errors.full_messages).to include("Name can't be blank")
     end
 
-    # validates :price, presence: true
     it 'returns an error when :price_cents is not present' do
       category = Category.new(name: 'x')
       product = Product.new(name: 'y', price_cents: nil, quantity: 10, category: category)
@@ -25,7 +22,6 @@ RSpec.describe Product, type: :model do
       expect(product.errors.full_messages).to include("Price can't be blank")
     end
 
-    # validates :quantity, presence: true
     it 'returns an error when :quantity is not present' do
       category = Category.new(name: 'x')
       product = Product.new(name: 'y', price_cents: 500, quantity: nil, category: category)
@@ -33,7 +29,6 @@ RSpec.describe Product, type: :model do
       expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
 
-    # validates :category, presence: true
     it 'returns an error when :price is not present' do
       category = Category.new(name: 'x')
       product = Product.new(name: 'y', price_cents: 500, quantity: 10, category: nil)
