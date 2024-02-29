9d32b51a8fe3934b182d7480e3226cce427990cf
maddie <lee.maddie@gmail.com>
2024-02-28 21:25:40 -0500

    Write password field validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,19 @@ RSpec.describe User, type: :model do
   end
 
   # must be created with password and password_confirmation fields
+  describe 'Password Validation' do
+    it 'returns an error when password is not present' do
+      user = User.new(
+      first_name: 'A',
+      last_name: 'B',
+      email: 'C@D.com',
+      password: '',
+      password_confirmation: 'EEEEEEE'
+    )
+    expect(user).to_not be_valid
+    expect(user.errors.full_messages).to include("Password can't be blank")
+    end
+  end
   # emails must be unique, NOT case sensitive
   # email, first name, last name all required
 end
