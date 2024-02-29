160609769fc6a23492a7aab497ec470069cadc0a
maddie <lee.maddie@gmail.com>
2024-02-28 21:05:05 -0500

    Write validation test for product with all attributes

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    #examples
+    # initial example
+    it 'ensures a product with all four fields setwill save successfully' do
+      category = Category.new(name: 'x')
+      category.save
+      product = Product.new(name: 'y', price: 500, quantity: 10, category: category)
+      product.save
+      expect(product).to be_valid
+    end
+
+    # validates :name, presence: true
+    # validates :price, presence: true
+    # validates :quantity, presence: true
+    # validates :category, presence: true
   end
 end
