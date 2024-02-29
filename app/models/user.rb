3cb00ff6d8bf1cf35643e9156b0fe2c3164d60cd
maddie <lee.maddie@gmail.com>
2024-02-28 23:04:25 -0500

    Add minimum length validation to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, confirmation: true
+  validates :password, length: { minimum: 7 }
 end
