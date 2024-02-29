2ba182b77cd17742522da435146ead82a864b350
maddie <lee.maddie@gmail.com>
2024-02-28 21:47:09 -0500

    Add email uniqueness to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true
   validates :email, uniqueness: true
+  validates :email, uniqueness: { case_sensitive: false }
   validates :password, confirmation: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_22_165736) do
+ActiveRecord::Schema.define(version: 2024_02_29_023929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2024_02_22_165736) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
+    t.string "email", unique: true
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
