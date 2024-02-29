7c8fae76101f3be351947167d4641632287b9081
maddie <lee.maddie@gmail.com>
2024-02-28 21:20:55 -0500

    Add requirements for testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # validates a user with all fields
+  # must be created with password and password_confirmation fields
+  # emails must be unique, NOT case sensitive
+  # email, first name, last name all required
 end
