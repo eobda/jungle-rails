3131983bc68f979f3d8321700c6cf97536447d69
maddie <lee.maddie@gmail.com>
2024-02-28 21:10:22 -0500

    Fix name of attribute price_cents

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,19 +5,26 @@ RSpec.describe Product, type: :model do
     # initial example
     it 'ensures a product with all four fields set will save successfully' do
       category = Category.new(name: 'x')
-      product = Product.new(name: 'y', price: 500, quantity: 10, category: category)
+      product = Product.new(name: 'y', price_cents: 500, quantity: 10, category: category)
       expect(product).to be_valid
     end
 
     # validates :name, presence: true
     it 'returns an error when :name is not present' do
       category = Category.new(name: 'x')
-      product = Product.new(name: nil, price: 500, quantity: 10, category: category)
+      product = Product.new(name: nil, price_cents: 500, quantity: 10, category: category)
       expect(product).to_not be_valid
       expect(product.errors.full_messages).to include("Name can't be blank")
     end
 
     # validates :price, presence: true
+    it 'returns an error when :price is not present' do
+      category = Category.new(name: 'x')
+      product = Product.new(name: 'y', price_cents: nil, quantity: 10, category: category)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
+    end
+
     # validates :quantity, presence: true
     # validates :category, presence: true
   end
