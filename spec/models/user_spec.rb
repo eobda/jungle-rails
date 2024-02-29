07d0da188133ec3e7d6cf6d01d9dfbbab949ff82
maddie <lee.maddie@gmail.com>
2024-02-28 23:48:02 -0500

    Write test expecting nil for bad user credentials

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -154,5 +154,18 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
       expect(user.authenticate_with_credentials('C@D.com', 'EEEEEEE')).to_not be nil
     end
+
+    it 'returns nil if user credentials do not exist in database' do
+      user = User.new(
+        first_name: 'A',
+        last_name: 'B',
+        email: 'C@D.com',
+        password: 'EEEEEEE',
+        password_confirmation: 'EEEEEEE'
+      )
+      user.save
+      expect(user).to be_valid
+      expect(user.authenticate_with_credentials('A@B.com', 'CCCCCCC')).to be nil
+    end
   end
 end
