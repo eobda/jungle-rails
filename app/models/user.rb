9478d45caaa387758955d1e349341185f47eb290
maddie <lee.maddie@gmail.com>
2024-02-29 02:08:34 -0500

    Use normalize_email method to ensure emails are always saved as the same case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save :normalize_email
+
   has_secure_password
   
   validates :first_name, presence: true
@@ -12,14 +14,20 @@ class User < ApplicationRecord
     if email.strip
       email.strip!
     else
-      email = email
+      email
     end
 
-    user = User.find_by(email: email)
+    user = User.find_by(email: email.downcase)
     if user && user.authenticate(password)
       user
     else
       nil
     end
+  end
+
+  private
+
+  def normalize_email
+    self.email = email.downcase
   end
 end
