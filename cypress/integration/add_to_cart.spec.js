377bbd4e1a7ab2bd26b6b4481d99e2f964a37483
maddie <lee.maddie@gmail.com>
2024-03-03 12:10:35 -0500

    Set up code for add to cart test

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,10 @@
+describe("home page tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("shows products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+});
