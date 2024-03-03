5cd06d7ad4f18e1055fd6c2d5d32264dba6c5848
maddie <lee.maddie@gmail.com>
2024-03-02 21:30:55 -0500

    Add test for two products

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -7,4 +7,8 @@ describe("home page tests", () => {
     cy.get(".products article").should("be.visible");
   });
 
+  it("shows 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
 });
