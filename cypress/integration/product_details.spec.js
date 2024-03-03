b6a4e4a15c584223442a7143b35255452e5ccde6
maddie <lee.maddie@gmail.com>
2024-03-03 12:05:30 -0500

    Write test to navigate to product page

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,7 +1,9 @@
-describe("Product detils page tests", () => {
+describe("Product details page tests", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
 
-
+  it("navigates to a product partial", () => {
+    cy.get('.products article').first().click();
+  });
 });
