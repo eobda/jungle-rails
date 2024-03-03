8074208f43d44a55bf3af1389e2d565444a539ef
maddie <lee.maddie@gmail.com>
2024-03-03 12:08:20 -0500

    Write assertion test to ensure page loads correctly

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -5,5 +5,6 @@ describe("Product details page tests", () => {
 
   it("navigates to a product partial", () => {
     cy.get('.products article').first().click();
+    cy.get('.product-detail div h1').should('contain.text', 'Scented Blade');
   });
 });
